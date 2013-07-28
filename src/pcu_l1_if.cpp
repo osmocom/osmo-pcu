f14ddb7830e873c759a26d58da57467a74cc44b9
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2013-07-28 15:59:46 +0200

    pcu_l1_if.cpp: Remove unused method and comment
    pcu_l1_if.cpp:195:13: warning: 'void pcu_l1if_tx_bcch(uint8_t*, int)' defined but not used [-Wunused-function]

--- src/pcu_l1_if.cpp
+++ src/pcu_l1_if.cpp
@@ -191,12 +191,6 @@ void pcu_l1if_tx_pch(bitvec * block, int plen, char *imsi)
 	pcu_tx_data_req(0, 0, PCU_IF_SAPI_PCH, 0, 0, 0, data, 23+3);
 }
 
-// FIXME: remove this, when changed from c++ to c.
-static void pcu_l1if_tx_bcch(uint8_t *data, int len)
-{
-	pcu_tx_data_req(0, 0, PCU_IF_SAPI_BCCH, 0, 0, 0, data, len);
-}
-
 extern "C" int pcu_rx_data_ind_pdtch(uint8_t trx, uint8_t ts, uint8_t *data,
 	uint8_t len, uint32_t fn, int8_t rssi)
 {
