be9a5589f5cabfa8476a9eb7542e6f07b99c2cb1
Jacob Erlbeck <jerlbeck@sysmocom.de>
2016-02-02 15:37:02 +0100

    Revert "WIP: Use current_pacch_slots (TODO)"
    This reverts commit 02fddb6c964d797926006898346a0192c6ac41f9.

--- src/tbf.cpp
+++ src/tbf.cpp
@@ -378,7 +378,6 @@ int gprs_rlcmac_tbf::update()
 	return 0;
 }
 
-/* TODO: Remove me */
 int tbf_assign_control_ts(struct gprs_rlcmac_tbf *tbf)
 {
 	if (tbf->control_ts == 0xff)
@@ -926,11 +925,10 @@ struct msgb *gprs_rlcmac_tbf::create_dl_ass(uint32_t fn, uint8_t ts)
 	if (direction == GPRS_RLCMAC_DL_TBF && !is_control_ts(ts)) {
 		LOGP(DRLCMAC, LOGL_NOTICE, "Cannot poll for downlink "
 			"assigment, because MS cannot reply. (TS=%d, "
-			"first control TS=%d)\n", ts,
-			first_control_ts());
+			"first common TS=%d)\n", ts,
+			first_common_ts);
 		poll_ass_dl = 0;
 	}
-
 	if (poll_ass_dl) {
 		if (poll_state == GPRS_RLCMAC_POLL_SCHED &&
 			ul_ass_state == GPRS_RLCMAC_UL_ASS_WAIT_ACK)
@@ -1239,12 +1237,11 @@ uint8_t gprs_rlcmac_tbf::ul_slots() const
 	size_t i;
 
 	if (direction == GPRS_RLCMAC_DL_TBF) {
-#if 0 /* This depends on the existence of an concurrent TBF */
 		if (control_ts < 8)
 			slots |= 1 << control_ts;
 		if (first_common_ts < 8)
 			slots |= 1 << first_common_ts;
-#endif
+
 		return slots;
 	}
 
@@ -1257,25 +1254,10 @@ uint8_t gprs_rlcmac_tbf::ul_slots() const
 
 bool gprs_rlcmac_tbf::is_control_ts(uint8_t ts) const
 {
-	if (!ms())
-		return ts == control_ts;
-
-	return ms()->current_pacch_slots() & (1 << ts);
+	return ts == control_ts;
 }
 
 uint8_t gprs_rlcmac_tbf::first_control_ts() const
 {
-	uint8_t bitnum;
-
-	if (!ms())
-		return first_ts;
-
-	bitnum = ffs(ms()->current_pacch_slots());
-
-	if (bitnum == 0) {
-		LOGP(DRLCMAC, LOGL_ERROR, "%s: No valid PACCH slot\n", name());
-		return first_ts;
-	}
-
-	return bitnum - 1;
+	return first_ts;
 }
