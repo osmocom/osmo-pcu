0ba0d186a558a9bcf75fabdb4649970439a58c29
Max <msuraev@sysmocom.de>
2016-02-24 17:35:32 +0100

    Explicitly initialize m_v_n and m_v_b
    Call reset() to initialize m_v_b in gprs_rlc_dl_window() and
    m_v_n in gprs_rlc_ul_window() constructors.
    
    Fixes: Coverity: CID 1351738, 1351737

--- src/rlc.h
+++ src/rlc.h
@@ -448,6 +448,7 @@ inline gprs_rlc_dl_window::gprs_rlc_dl_window()
 	: m_v_s(0)
 	, m_v_a(0)
 {
+	m_v_b.reset();
 }
 
 inline const uint16_t gprs_rlc_dl_window::v_s() const
@@ -494,6 +495,7 @@ inline gprs_rlc_ul_window::gprs_rlc_ul_window()
 	: m_v_r(0)
 	, m_v_q(0)
 {
+	m_v_n.reset();
 }
 
 inline bool gprs_rlc_ul_window::is_in_window(uint16_t bsn) const
