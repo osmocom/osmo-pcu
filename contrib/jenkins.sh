7fd177b91c5b0cb32078d87b41da5ad38c2f08ad
Neels Hofmeyr <neels@hofmeyr.de>
2016-08-10 15:29:31 +0200

    jenkins.sh: change build matrix to $with_dsp and $with_vty
    The new $with_dsp matrix parameter is defined as "sysmo" or empty/"none". The
    lc15 DSP might be added in the future.
    
    Fetch the sysmo layer 1 API only if with_dsp==sysmo.
    
    The new $with_vty parameter is independent of $with_dsp, it is now up to
    jenkins to define a matrix filter.
    
    For compat, until jenkins is reconfigured with the new matrix parameters, use
    $sysmodsp to init the new parameters to reflect previous behavior. The
    $sysmobts matrix parameter made no sense, drop it.
    
    Change-Id: Ia120f918342dc9563814252258b73bfb267e5253

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -7,10 +7,14 @@ if [ -z "$MAKE" ]; then
   exit 1
 fi
 
-if [ "$sysmobts" = "no" -a "$sysmodsp" = "yes" ]; then
-  echo "This config does not make sense."
-  exit 0
+# Compat: to be able to smoothly reconfigure the jenkins job, handle both
+# old and new matrix variables
+if [ -z "$with_dsp" -a "$sysmodsp" = yes ]; then
+  with_dsp="sysmo"
+else
+  with_vty="yes"
 fi
+# end of compat part
 
 base="$PWD"
 deps="$base/deps"
@@ -19,6 +23,40 @@ inst="$deps/install"
 rm -rf "$inst"
 mkdir -p "$deps"
 
+# Collect configure options for osmo-pcu
+PCU_CONFIG=""
+if [ "$with_dsp" = sysmo ]; then
+  PCU_CONFIG="$PCU_CONFIG --enable-sysmocom-dsp"
+
+  # For direct sysmo DSP access, provide the SysmoBTS Layer 1 API
+  cd "$deps"
+  if [ ! -d layer1-api ]; then
+    git clone git://git.sysmocom.de/sysmo-bts/layer1-api.git layer1-api
+  fi
+  cd layer1-api
+  git fetch origin
+  git reset --hard origin/master
+  api_incl="$inst/include/sysmocom/femtobts/"
+  mkdir -p "$api_incl"
+  cp include/*.h "$api_incl"
+
+elif [ -z "$with_dsp" -o "$with_dsp" = none ]; then
+  echo "Direct DSP access disabled"
+else
+  echo 'Invalid $with_dsp value:' $with_dsp
+  exit 1
+fi
+
+if [ "$with_vty" = "yes" ]; then
+  PCU_CONFIG="$PCU_CONFIG --enable-vty-tests"
+elif [ -z "$with_vty" -o "$with_vty" = "no" ]; then
+  echo "VTY tests disabled"
+else
+  echo 'Invalid $with_vty value:' $with_vty
+  exit 1
+fi
+
+# Build deps
 cd "$deps"
 osmo-deps.sh libosmocore
 cd libosmocore
@@ -26,24 +64,10 @@ autoreconf --install --force
 ./configure --prefix="$inst"
 $MAKE $PARALLEL_MAKE install
 
-cd "$deps"
-if [ ! -d layer1-api ]; then
-  git clone git://git.sysmocom.de/sysmo-bts/layer1-api.git layer1-api
-fi
-cd layer1-api
-git fetch origin
-git reset --hard origin/master
-api_incl="$inst/include/sysmocom/femtobts/"
-mkdir -p "$api_incl"
-cp include/*.h "$api_incl"
-
-PCU_CONFIG="--enable-sysmocom-bts=$sysmobts --enable-sysmocom-dsp=$sysmodsp"
-if [ "$sysmobts" = "no" ]; then
-  PCU_CONFIG="$PCU_CONFIG --enable-vty-tests"
-fi
-
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig"
 export LD_LIBRARY_PATH="$inst/lib"
+
+# Build osmo-pcu
 cd "$base"
 autoreconf --install --force
 ./configure $PCU_CONFIG
