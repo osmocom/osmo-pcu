fd9e16ce97603d2dbc692ebc6e6f27b966ceca25
Neels Hofmeyr <neels@hofmeyr.de>
2016-08-23 16:43:05 +0200

    heed VTY 'line vty'/'bind' command
    Like most other osmo-* programs, bind the telnet VTY to the address specified
    by the 'line vty'/'bind' command. This is added by vty_init(), so until now the
    PCU offered this config but ignored it.
    
    Change-Id: I4cca05a212ec0d493b906014dc3a83e687ebbb1d

--- src/pcu_main.cpp
+++ src/pcu_main.cpp
@@ -251,7 +251,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "No config file: '%s' Using default config.\n",
 			config_file);
 
-	rc = telnet_init(tall_pcu_ctx, NULL, OSMO_VTY_PORT_PCU);
+	rc = telnet_init_dynif(tall_pcu_ctx, NULL, vty_get_bind_addr(),
+			       OSMO_VTY_PORT_PCU);
 	if (rc < 0) {
 		fprintf(stderr, "Error initializing telnet\n");
 		exit(1);
