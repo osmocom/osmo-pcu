8491ded354fc033ae3d3c2677951d037c1fa3619
Alexander Couzens <lynxis@fe80.eu>
2016-12-02 17:50:20 +0100

    pcu_l1_if: use sizeof intead of hardcoded size value
    Change-Id: If53ec3a48208ceca53386e61219246743b655fb0

--- src/pcu_l1_if.cpp
+++ src/pcu_l1_if.cpp
@@ -169,7 +169,7 @@ void pcu_l1if_tx_agch(bitvec * block, int plen)
 	/* FIXME: why does OpenBTS has no PLEN and no fill in message? */
 	bitvec_pack(block, data + 1);
 	data[0] = (plen << 2) | 0x01;
-	pcu_tx_data_req(0, 0, PCU_IF_SAPI_AGCH, 0, 0, 0, data, 23);
+	pcu_tx_data_req(0, 0, PCU_IF_SAPI_AGCH, 0, 0, 0, data, sizeof(data));
 }
 
 void pcu_l1if_tx_pch(bitvec * block, int plen, const char *imsi)
