4cb6e04914d7d66b3debdfb4e62ff64d8613deff
Max <msuraev@sysmocom.de>
2018-01-17 16:14:59 +0100

    jenkins.sh: Disable building doxygen for deps
    Don't clutter build logs with irrelevant output.
    
    Change-Id: If1784baa519c10ab0ab3e600f373c27a6c8ae4c6

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -61,7 +61,7 @@ fi
 verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 
 # Build deps
-osmo-build-dep.sh libosmocore
+osmo-build-dep.sh libosmocore "" --disable-doxygen
 
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
