osmocom/osmo-pcu

commit 9d1cdb1f697057033394590a9b2815efe6c08cd9

Move out tbf subclasses from tbf.h to their own headers
It's a good start towards clearing current mess between parent and the 2
children classes.

Change-Id: Ibc22ea2e02609af7ee058b8bc15df2115d4c6f60

src/tbf.cpp
@@ -21,6 +21,8 @@
 
 #include <bts.h>
 #include <tbf.h>
+#include <tbf_dl.h>
+#include <tbf_ul.h>
 #include <rlc.h>
 #include <encoding.h>
 #include <gprs_rlcmac.h>

src/tbf_dl.cpp
@@ -21,6 +21,8 @@
 
 #include <bts.h>
 #include <tbf.h>
+#include <tbf_dl.h>
+#include <tbf_ul.h>
 #include <rlc.h>
 #include <gprs_rlcmac.h>
 #include <gprs_debug.h>

src/tbf_ul.cpp
@@ -21,6 +21,7 @@
 
 #include <bts.h>
 #include <tbf.h>
+#include <tbf_ul.h>
 #include <rlc.h>
 #include <encoding.h>
 #include <gprs_rlcmac.h>

src/Makefile.am
@@ -91,6 +91,8 @@ noinst_HEADERS = \
 	pcu_vty_functions.h \
 	mslot_class.h \
 	tbf.h \
+	tbf_ul.h \
+	tbf_dl.h \
 	bts.h \
 	pdch.h \
 	poll_controller.h \

src/bts.cpp
@@ -21,6 +21,7 @@
 #include <bts.h>
 #include <poll_controller.h>
 #include <tbf.h>
+#include <tbf_ul.h>
 #include <encoding.h>
 #include <decoding.h>
 #include <rlc.h>

src/encoding.cpp
@@ -23,6 +23,7 @@
 #include <gprs_rlcmac.h>
 #include <bts.h>
 #include <tbf.h>
+#include <tbf_ul.h>
 #include <gprs_debug.h>
 #include <egprs_rlc_compression.h>
 

src/gprs_ms.cpp
@@ -23,6 +23,7 @@
 #include <gprs_coding_scheme.h>
 #include "bts.h"
 #include "tbf.h"
+#include "tbf_ul.h"
 #include "gprs_debug.h"
 #include "gprs_codel.h"
 #include "pcu_utils.h"

src/gprs_ms.h
@@ -26,6 +26,7 @@ struct gprs_codel;
 #include "cxx_linuxlist.h"
 #include "llc.h"
 #include "tbf.h"
+#include "tbf_dl.h"
 #include "pcu_l1_if.h"
 #include <gprs_coding_scheme.h>
 

commit bddf1ad7f6229ae5a8091274fc50036e9236418c

Move tbf_{dl,ul} child constructors to respective .cpp files
Fixes: 9d1cdb1f697057033394590a9b2815efe6c08cd9
Change-Id: Id258589d46de42ad4e27889bc396f930b7f94b79

src/tbf.cpp
@@ -955,17 +955,6 @@ static int setup_tbf(struct gprs_rlcmac_tbf *tbf, GprsMs *ms, int8_t use_trx, ui
 	return 0;
 }
 
-gprs_rlcmac_ul_tbf::gprs_rlcmac_ul_tbf(BTS *bts_) :
-	gprs_rlcmac_tbf(bts_, GPRS_RLCMAC_UL_TBF),
-	m_rx_counter(0),
-	m_contention_resolution_done(0),
-	m_final_ack_sent(0),
-	m_ul_gprs_ctrs(NULL),
-	m_ul_egprs_ctrs(NULL)
-{
-	memset(&m_usf, 0, sizeof(m_usf));
-}
-
 static int ul_tbf_dtor(struct gprs_rlcmac_ul_tbf *tbf)
 {
 	tbf->~gprs_rlcmac_ul_tbf();
@@ -1052,19 +1041,6 @@ gprs_rlcmac_dl_tbf::BandWidth::BandWidth() :
 {
 	timespecclear(&dl_bw_tv);
 	timespecclear(&dl_loss_tv);
-}
-
-gprs_rlcmac_dl_tbf::gprs_rlcmac_dl_tbf(BTS *bts_) :
-	gprs_rlcmac_tbf(bts_, GPRS_RLCMAC_DL_TBF),
-	m_tx_counter(0),
-	m_wait_confirm(0),
-	m_dl_ack_requested(false),
-	m_last_dl_poll_fn(0),
-	m_last_dl_drained_fn(0),
-	m_dl_gprs_ctrs(NULL),
-	m_dl_egprs_ctrs(NULL)
-{
-	memset(&m_llc_timer, 0, sizeof(m_llc_timer));
 }
 
 static int dl_tbf_dtor(struct gprs_rlcmac_dl_tbf *tbf)

src/tbf_dl.cpp
@@ -76,6 +76,19 @@ static void llc_timer_cb(void *_tbf)
 	tbf->request_dl_ack();
 }
 
+gprs_rlcmac_dl_tbf::gprs_rlcmac_dl_tbf(BTS *bts_) :
+	gprs_rlcmac_tbf(bts_, GPRS_RLCMAC_DL_TBF),
+	m_tx_counter(0),
+	m_wait_confirm(0),
+	m_dl_ack_requested(false),
+	m_last_dl_poll_fn(0),
+	m_last_dl_drained_fn(0),
+	m_dl_gprs_ctrs(NULL),
+	m_dl_egprs_ctrs(NULL)
+{
+	memset(&m_llc_timer, 0, sizeof(m_llc_timer));
+}
+
 void gprs_rlcmac_dl_tbf::cleanup()
 {
 	osmo_timer_del(&m_llc_timer);

src/tbf_ul.cpp
@@ -55,6 +55,17 @@ extern "C" {
 
 extern void *tall_pcu_ctx;
 
+gprs_rlcmac_ul_tbf::gprs_rlcmac_ul_tbf(BTS *bts_) :
+	gprs_rlcmac_tbf(bts_, GPRS_RLCMAC_UL_TBF),
+	m_rx_counter(0),
+	m_contention_resolution_done(0),
+	m_final_ack_sent(0),
+	m_ul_gprs_ctrs(NULL),
+	m_ul_egprs_ctrs(NULL)
+{
+	memset(&m_usf, 0, sizeof(m_usf));
+}
+
 /*
  * Store received block data in LLC message(s) and forward to SGSN
  * if complete.