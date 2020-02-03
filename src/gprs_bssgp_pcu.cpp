3568fcfa7d3023df79e6c1a2d8135952943e6329
Vadim Yanitskiy <axilirator@gmail.com>
2020-02-03 16:38:51 +0700

    gprs_bssgp_pcu: fixup: fix length check in gprs_bssgp_pcu_rx_dl_ud()
    Change-Id: I7d49b27a615350a7707154aa3cc903db7c1df374
    Fixes: I7f84bd776cc780a45880f136107f6e0bc56241d1

--- src/gprs_bssgp_pcu.cpp
+++ src/gprs_bssgp_pcu.cpp
@@ -108,7 +108,7 @@ static int gprs_bssgp_pcu_rx_dl_ud(struct msgb *msg, struct tlv_parsed *tp)
 
 	data = (uint8_t *) TLVP_VAL(tp, BSSGP_IE_LLC_PDU);
 	len = TLVP_LEN(tp, BSSGP_IE_LLC_PDU);
-	if (len > sizeof(LLC_MAX_LEN))
+	if (len > LLC_MAX_LEN)
 	{
 		LOGP(DBSSGP, LOGL_NOTICE, "BSSGP TLLI=0x%08x Rx UL-UD IE_LLC_PDU too large\n", tlli);
 		return bssgp_tx_status(BSSGP_CAUSE_COND_IE_ERR, NULL, msg);
