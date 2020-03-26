a0cbde700a53bbc804552bbbd1243fe9c6510443
Pau Espin Pedrol <pespin@sysmocom.de>
2020-03-26 15:56:15 +0100

    tbf.cpp: Include c++ <new> header required for new operator's replacement type
    Including the <new> header is required as explained by the c++ specs [1]
    
    osmo-pcu/src/tbf.cpp: In function ‘gprs_rlcmac_ul_tbf* tbf_alloc_ul_tbf(gprs_rlcmac_bts*, GprsMs*, int8_t, uint8_t, uint8_t, bool)’:
    osmo-pcu/src/tbf.cpp:1002:39: error: no matching function for call to ‘operator new(sizetype, gprs_rlcmac_ul_tbf*&)’
     1002 |  new (tbf) gprs_rlcmac_ul_tbf(bts->bts);
          |                                       ^
    
    Most of the times this issue is not detected because other STL headers
    are already including <new>.
    
    [1] http://www.cplusplus.com/reference/new/operator%20new/
    
    Change-Id: Ie5fb536ae29dcf40e2a0dbe67432bebd61b8c7aa

--- src/tbf.cpp
+++ src/tbf.cpp
@@ -19,6 +19,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <new>
+
 #include <bts.h>
 #include <tbf.h>
 #include <tbf_dl.h>
--- tests/ms/MsTest.cpp
+++ tests/ms/MsTest.cpp
@@ -40,6 +40,8 @@ extern "C" {
 #include <errno.h>
 #include <unistd.h>
 
+#include <new>
+
 void *tall_pcu_ctx;
 int16_t spoof_mnc = 0, spoof_mcc = 0;
 bool spoof_mnc_3_digits = false;
