osmocom/osmo-pcu

commit c0a250d17d79e08763f77cdf58e970dc2e65e7d3

Introduce NACC support
A new nacc_fsm is introduced per MS object, with its partner priv
structure struct nacc_fsm_ctx, which exists and is available in the MS
object only during the duration of the NACC procedure.

The NACC context is created on an MS whenever a Pkt Cell Change
Notification is received on Uplink RLCMAC, which asks for neighbor
information of a given ARFCN+BSIC.

First, the target ARFCN+BSIC needs to be translated into a CGI-PS
(RAC+CI) address. That's done by asking the BSC through the Neighbour
Resolution Service available in osmo-bsc using the CTRL interface.

Once the CGI-PS of the target cell is known, PCU starts a RIM RAN-INFO
request against the SGSN (which will route the request as needed), and
wait for a response containing the SI bits from the target cell.

After the SI are received, the scheduler is instructed to eventually
poll a TBF for the MS originating the CCN, so that we can send the SI
encapsulated into multiple Packet Neighbor Cell Data messages on the
downlink.

One all the SI bits are sent, the scheduler is instructed to send a
Packet Cell Change Continue message.

Once the message above has been sent, the FSM autodestroys itself.

Caches are also introduced in this patch which allows for re-using
recently known translations ARFCN+BSIC -> CGI-PS and CGI-PS -> SI_INFO
respectively.

Change-Id: Id35f40d05f3e081f32fddbf1fa34cb338db452ca

src/pcu_vty.c
@@ -14,6 +14,7 @@
 #include <osmocom/vty/misc.h>
 #include <osmocom/core/linuxlist.h>
 #include <osmocom/core/rate_ctr.h>
+#include <osmocom/ctrl/ports.h>
 #include <osmocom/pcu/pcuif_proto.h>
 #include <osmocom/gprs/gprs_ns2.h>
 #include "pcu_vty.h"
@@ -1018,6 +1019,22 @@ DEFUN_USRATTR(cfg_pcu_gb_dialect,
 	return CMD_SUCCESS;
 }
 
+DEFUN(cfg_neighbor_resolution, cfg_neighbor_resolution_cmd,
+       "neighbor resolution " VTY_IPV46_CMD " [<0-65535>]",
+       "Manage local and remote-BSS neighbor cells\n"
+       "Connect to Neighbor Resolution Service (CTRL interface) to given ip and port\n"
+       "IPv4 address to connect to\n" "IPv6 address to connect to\n"
+       "Port to connect to (default 4248)\n")
+{
+	osmo_talloc_replace_string(the_pcu, &the_pcu->vty.neigh_ctrl_addr, argv[0]);
+	if (argc > 1)
+		the_pcu->vty.neigh_ctrl_port = atoi(argv[1]);
+	else
+		the_pcu->vty.neigh_ctrl_port = OSMO_CTRL_PORT_BSC_NEIGH;
+	return CMD_SUCCESS;
+}
+
+
 DEFUN(show_bts_timer, show_bts_timer_cmd,
       "show bts-timer " OSMO_TDEF_VTY_ARG_T_OPTIONAL,
       SHOW_STR "Show BTS controlled timers\n"
@@ -1220,6 +1237,7 @@ int pcu_vty_init(void)
 	install_element(PCU_NODE, &cfg_pcu_no_gsmtap_categ_cmd);
 	install_element(PCU_NODE, &cfg_pcu_sock_cmd);
 	install_element(PCU_NODE, &cfg_pcu_gb_dialect_cmd);
+	install_element(PCU_NODE, &cfg_neighbor_resolution_cmd);
 	install_element(PCU_NODE, &cfg_pcu_timer_cmd);
 
 	install_element_ve(&show_bts_stats_cmd);

configure.ac
@@ -84,6 +84,7 @@ fi
 dnl checks for libraries
 PKG_CHECK_MODULES(LIBOSMOCORE, libosmocore >= 1.4.0)
 PKG_CHECK_MODULES(LIBOSMOVTY, libosmovty >= 1.4.0)
+PKG_CHECK_MODULES(LIBOSMOCTRL, libosmoctrl >= 1.4.0)
 PKG_CHECK_MODULES(LIBOSMOGSM, libosmogsm >= 1.4.0)
 PKG_CHECK_MODULES(LIBOSMOGB, libosmogb >= 1.4.0)
 

src/Makefile.am
@@ -19,7 +19,7 @@
 #
 
 AUTOMAKE_OPTIONS = subdir-objects
-AM_CPPFLAGS = -I$(top_srcdir)/include $(STD_DEFINES_AND_INCLUDES) $(LIBOSMOCORE_CFLAGS) $(LIBOSMOGB_CFLAGS) $(LIBOSMOGSM_CFLAGS)
+AM_CPPFLAGS = -I$(top_srcdir)/include $(STD_DEFINES_AND_INCLUDES) $(LIBOSMOCORE_CFLAGS) $(LIBOSMOGB_CFLAGS) $(LIBOSMOCTRL_CFLAGS) $(LIBOSMOGSM_CFLAGS)
 
 if ENABLE_SYSMODSP
 AM_CPPFLAGS += -DENABLE_DIRECT_PHY
@@ -55,6 +55,8 @@ libgprs_la_SOURCES = \
 	pcu_vty.c \
 	pcu_vty_functions.cpp \
 	mslot_class.c \
+	nacc_fsm.c \
+	neigh_cache.c \
 	tbf.cpp \
 	tbf_ul.cpp \
 	tbf_dl.cpp \
@@ -91,6 +93,8 @@ noinst_HEADERS = \
 	pcu_vty.h \
 	pcu_vty_functions.h \
 	mslot_class.h \
+	nacc_fsm.h \
+	neigh_cache.h \
 	tbf.h \
 	tbf_ul.h \
 	tbf_dl.h \
@@ -143,6 +147,7 @@ osmo_pcu_remote_LDADD = \
 	libgprs.la \
 	$(LIBOSMOGB_LIBS) \
 	$(LIBOSMOCORE_LIBS) \
+	$(LIBOSMOCTRL_LIBS) \
 	$(LIBOSMOGSM_LIBS) \
 	$(COMMON_LA)
 endif
@@ -191,6 +196,7 @@ osmo_pcu_LDADD = \
 	libgprs.la \
 	$(LIBOSMOGB_LIBS) \
 	$(LIBOSMOCORE_LIBS) \
+	$(LIBOSMOCTRL_LIBS) \
 	$(LIBOSMOGSM_LIBS) \
 	$(COMMON_LA)
 

src/bts.cpp
@@ -140,6 +140,9 @@ static const struct rate_ctr_desc bts_ctr_description[] = {
 	{ "pkt:ul_assignment",		"Packet UL Assignment "},
 	{ "pkt:access_reject",          "Packet Access Reject "},
 	{ "pkt:dl_assignment",		"Packet DL Assignment "},
+	{ "pkt:cell_chg_notification",	"Packet Cell Change Notification"},
+	{ "pkt:cell_chg_continue",	"Packet Cell Change Continue"},
+	{ "pkt:neigh_cell_data",	"Packet Neighbour Cell Data"},
 	{ "ul:control",			"UL control Block     "},
 	{ "ul:assignment_poll_timeout",	"UL Assign Timeout    "},
 	{ "ul:assignment_failed",	"UL Assign Failed     "},
@@ -1283,4 +1286,9 @@ void bts_recalc_max_mcs(struct gprs_rlcmac_bts *bts)
 struct GprsMs *bts_ms_by_imsi(struct gprs_rlcmac_bts *bts, const char *imsi)
 {
 	return bts_ms_store(bts)->get_ms(0, 0, imsi);
+}
+
+const struct llist_head* bts_ms_list(struct gprs_rlcmac_bts *bts)
+{
+	return bts_ms_store(bts)->ms_list();
 }

src/bts.h
@@ -2,6 +2,7 @@
  *
  * Copyright (C) 2012 Ivan Klyuchnikov
  * Copyright (C) 2013 by Holger Hans Peter Freyther
+ * Copyright (C) 2021 by sysmocom - s.f.m.c. GmbH <info@sysmocom.de
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -125,6 +126,9 @@ enum {
 	CTR_PKT_UL_ASSIGNMENT,
 	CTR_PKT_ACCESS_REJ,
 	CTR_PKT_DL_ASSIGNMENT,
+	CTR_PKT_CELL_CHG_NOTIFICATION,
+	CTR_PKT_CELL_CHG_CONTINUE,
+	CTR_PKT_NEIGH_CELL_DATA,
 	CTR_RLC_RECV_CONTROL,
 	CTR_PUA_POLL_TIMEDOUT,
 	CTR_PUA_POLL_FAILED,
@@ -337,6 +341,7 @@ void bts_set_max_cs_ul(struct gprs_rlcmac_bts *bts, uint8_t cs_ul);
 void bts_set_max_mcs_dl(struct gprs_rlcmac_bts *bts, uint8_t mcs_dl);
 void bts_set_max_mcs_ul(struct gprs_rlcmac_bts *bts, uint8_t mcs_ul);
 bool bts_cs_dl_is_supported(const struct gprs_rlcmac_bts *bts, enum CodingScheme cs);
+const struct llist_head* bts_ms_list(struct gprs_rlcmac_bts *bts);
 #ifdef __cplusplus
 }
 #endif

src/encoding.cpp
@@ -1731,3 +1731,56 @@ void Encoding::write_packet_access_reject(
 	bitvec_write_field(dest, &wp, 5, 8);  //  WAIT_INDICATION value
 	bitvec_write_field(dest, &wp, 0, 1);  //  WAIT_INDICATION size in seconds
 }
+
+void write_packet_neighbour_cell_data(RlcMacDownlink_t *block,
+		bool tfi_is_dl, uint8_t tfi, uint8_t container_id,
+		uint8_t container_idx, PNCDContainer_t *container)
+{
+
+	block->PAYLOAD_TYPE = 0x1;  // RLC/MAC control block that does not include the optional octets of the RLC/MAC control header
+	block->RRBP         = 0;  // 0: N+13
+	block->SP           = 0; // RRBP field is valid
+	block->USF          = 0x0;  // Uplink state flag
+
+	block->u.Packet_Neighbour_Cell_Data.MESSAGE_TYPE = MT_PACKET_NEIGHBOUR_CELL_DATA;
+	block->u.Packet_Neighbour_Cell_Data.PAGE_MODE    = 0x0;  // Normal Paging
+
+	block->u.Packet_Neighbour_Cell_Data.Global_TFI.UnionType = tfi_is_dl; // 0=UPLINK TFI, 1=DL TFI
+	if (tfi_is_dl) {
+		block->u.Packet_Neighbour_Cell_Data.Global_TFI.u.DOWNLINK_TFI = tfi;
+	} else {
+		block->u.Packet_Neighbour_Cell_Data.Global_TFI.u.UPLINK_TFI = tfi;
+	}
+	block->u.Packet_Neighbour_Cell_Data.CONTAINER_ID = container_id;
+	block->u.Packet_Neighbour_Cell_Data.spare = 0;
+	block->u.Packet_Neighbour_Cell_Data.CONTAINER_INDEX = container_idx;
+	block->u.Packet_Neighbour_Cell_Data.Container = *container;
+}
+
+void write_packet_cell_change_continue(RlcMacDownlink_t *block,
+		bool tfi_is_dl, uint8_t tfi, bool exist_id,
+		uint16_t arfcn, uint8_t bsic, uint8_t container_id)
+{
+
+	block->PAYLOAD_TYPE = 0x1;  // RLC/MAC control block that does not include the optional octets of the RLC/MAC control header
+	block->RRBP         = 0;  // 0: N+13
+	block->SP           = 0; // RRBP field is valid
+	block->USF          = 0x0;  // Uplink state flag
+
+	block->u.Packet_Cell_Change_Continue.MESSAGE_TYPE = MT_PACKET_CELL_CHANGE_CONTINUE;
+	block->u.Packet_Cell_Change_Continue.PAGE_MODE    = 0x0;  // Normal Paging
+
+	block->u.Packet_Cell_Change_Continue.Global_TFI.UnionType = tfi_is_dl; // 0=UPLINK TFI, 1=DL TFI
+	if (tfi_is_dl) {
+		block->u.Packet_Cell_Change_Continue.Global_TFI.u.DOWNLINK_TFI = tfi;
+	} else {
+		block->u.Packet_Cell_Change_Continue.Global_TFI.u.UPLINK_TFI = tfi;
+	}
+
+	block->u.Packet_Cell_Change_Continue.Exist_ID = exist_id;
+	if (exist_id) {
+		block->u.Packet_Cell_Change_Continue.ARFCN = arfcn;
+		block->u.Packet_Cell_Change_Continue.BSIC = bsic;
+	}
+	block->u.Packet_Cell_Change_Continue.CONTAINER_ID = container_id;
+}

src/encoding.h
@@ -22,17 +22,22 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 #include <osmocom/gsm/l1sap.h>
 #include "coding_scheme.h"
 #include "gsm_rlcmac.h"
+#ifdef __cplusplus
 }
+#endif
 
 struct gprs_rlcmac_tbf;
 struct bitvec;
 struct gprs_llc;
 struct gprs_rlc_data_block_info;
 
+#ifdef __cplusplus
 /**
  * I help with encoding data into CSN1 messages.
  * TODO: Nobody can remember a function signature like this. One should
@@ -108,3 +113,21 @@ public:
 		const struct gprs_rlc_data_block_info *rdbi,
 		int *offset, int *num_chunks, uint8_t *data_block);
 };
+
+#endif /* ifdef __cplusplus */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void write_packet_neighbour_cell_data(RlcMacDownlink_t *block,
+		bool tfi_is_dl, uint8_t tfi, uint8_t container_id,
+		uint8_t container_idx, PNCDContainer_t *container);
+
+void write_packet_cell_change_continue(RlcMacDownlink_t *block,
+		bool tfi_is_dl, uint8_t tfi, bool exist_id,
+		uint16_t arfcn, uint8_t bsic, uint8_t container_id);
+
+#ifdef __cplusplus
+}
+#endif

src/gprs_bssgp_rim.c
@@ -26,6 +26,9 @@
 
 #include "gprs_debug.h"
 #include "gprs_pcu.h"
+#include "bts.h"
+#include "gprs_ms.h"
+#include "nacc_fsm.h"
 
 #define LOGPRIM(nsei, level, fmt, args...) \
 	LOGP(DRIM, level, "(NSEI=%u) " fmt, nsei, ## args)
@@ -97,6 +100,65 @@ static bool match_app_id(const struct bssgp_ran_information_pdu *pdu, enum bssgp
 	return false;
 }
 
+static int handle_ran_info_response_nacc(const struct bssgp_ran_inf_app_cont_nacc *nacc, struct gprs_rlcmac_bts *bts)
+{
+	struct si_cache_value val;
+	struct si_cache_entry *entry;
+	struct llist_head *tmp;
+	int i;
+
+	LOGP(DRIM, LOGL_INFO, "Rx RAN-INFO cell=%s type=%sBCCH num_si=%d\n",
+	     osmo_cgi_ps_name(&nacc->reprt_cell),
+	     nacc->type_psi ? "P" : "", nacc->num_si);
+
+	val.type_psi = nacc->type_psi;
+	val.si_len = 0;
+	for (i = 0; i < nacc->num_si; i++) {
+		size_t len = val.type_psi ? BSSGP_RIM_PSI_LEN : BSSGP_RIM_SI_LEN;
+		memcpy(&val.si_buf[val.si_len], nacc->si[i], len);
+		val.si_len += len;
+	}
+	entry = si_cache_add(bts->pcu->si_cache, &nacc->reprt_cell, &val);
+
+	llist_for_each(tmp, bts_ms_list(bts)) {
+		struct GprsMs *ms = llist_entry(tmp, typeof(*ms), list);
+		if (!ms->nacc)
+			continue;
+		if (ms->nacc->fi->state != NACC_ST_WAIT_REQUEST_SI)
+			continue;
+		if (osmo_cgi_ps_cmp(&nacc->reprt_cell, &ms->nacc->cgi_ps) != 0)
+			continue;
+		osmo_fsm_inst_dispatch(ms->nacc->fi, NACC_EV_RX_SI, entry);
+	}
+	return 0;
+}
+
+static int handle_ran_info_response(const struct bssgp_ran_information_pdu *pdu, struct gprs_rlcmac_bts *bts)
+{
+	const struct bssgp_ran_inf_rim_cont *ran_info = &pdu->decoded.rim_cont;
+	char ri_src_str[64];
+
+	if (ran_info->app_err) {
+		LOGP(DRIM, LOGL_ERROR,
+		     "%s Rx RAN-INFO with an app error! cause: %s\n",
+		     bssgp_rim_ri_name_buf(ri_src_str, sizeof(ri_src_str), &pdu->routing_info_src),
+		     bssgp_nacc_cause_str(ran_info->u.app_err_cont_nacc.nacc_cause));
+		return -1;
+	}
+
+	switch (pdu->decoded.rim_cont.app_id) {
+	case BSSGP_RAN_INF_APP_ID_NACC:
+		handle_ran_info_response_nacc(&ran_info->u.app_cont_nacc, bts);
+		break;
+	default:
+		LOGP(DRIM, LOGL_ERROR, "%s Rx RAN-INFO with unknown/wrong application ID %s received\n",
+		     bssgp_rim_ri_name_buf(ri_src_str, sizeof(ri_src_str), &pdu->routing_info_src),
+		     bssgp_ran_inf_app_id_str(pdu->decoded.rim_cont.app_id));
+		return -1;
+	}
+	return 0;
+}
+
 int handle_rim(struct osmo_bssgp_prim *bp)
 {
 	struct msgb *msg = bp->oph.msg;
@@ -151,8 +213,7 @@ int handle_rim(struct osmo_bssgp_prim *bp)
 		LOGPRIM(nsei, LOGL_NOTICE, "Responding to RAN INFORMATION REQUEST not yet implemented!\n");
 		break;
 	case BSSGP_IE_RI_RIM_CONTAINER:
-		LOGPRIM(nsei, LOGL_NOTICE, "Responding to RAN INFORMATION not yet implemented!\n");
-		break;
+		return handle_ran_info_response(pdu, bts);
 	case BSSGP_IE_RI_APP_ERROR_RIM_CONT:
 	case BSSGP_IE_RI_ACK_RIM_CONTAINER:
 	case BSSGP_IE_RI_ERROR_RIM_COINTAINER:

commit 66e8a497345cc2c6ba375ac85158c3d0ab6f8736

vty: Write 'neighbor resolution' config to file
Fixes: c0a250d17d79e08763f77cdf58e970dc2e65e7d3
Related: SYS#4909
Change-Id: I44eef3826939e05ba88e0c5a67e1fef535582ba7

src/pcu_vty.c
@@ -250,6 +250,11 @@ static int config_write_pcu(struct vty *vty)
 	else
 		vty_out(vty, " gb-dialect classic%s", VTY_NEWLINE);
 
+	if (the_pcu->vty.neigh_ctrl_addr) {
+		vty_out(vty, " neighbor resolution %s %u%s",
+			the_pcu->vty.neigh_ctrl_addr, the_pcu->vty.neigh_ctrl_port, VTY_NEWLINE);
+	}
+
 	osmo_tdef_vty_write(vty, the_pcu->T_defs, " timer ");
 
 	return CMD_SUCCESS;