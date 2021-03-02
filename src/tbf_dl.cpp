osmocom/osmo-pcu

commit 7d0f9a0ec383fcfca934731bd6979b6be6629c90

Dl TBF: Get rid of LLC UI dummy blocks following other data
According to:
* 3GPP TS 44.060 version 16.0.0 "9.3.1a Delayed release of downlink Temporary Block Flow"
* 3GPP TS 44.064 version 16.0.0 "6.4.2.2 Unconfirmed Information (UI) Dummy command"

LLC UI Dummy frames are to be used when there no more data to send, only
in order to delay the release of a TBF. Hence, while not incorrect per
se, makes no sense to send those LLC UI Dummy frames inserted into
rlcmac blocks which already contain other LLC frames, since the MS in
that case is already being kept active.
It only makes sense to send those LLC UI Dummy frames when we have
nothing else to send, that is, alone inside a RLCMAC block without other
LLC frames.

Related: OS#4849
Change-Id: Ifae1a7b2b3dfad8df19585063088ba0df2749c8f

src/tbf_dl.cpp
@@ -694,9 +694,33 @@ int gprs_rlcmac_dl_tbf::create_new_bsn(const uint32_t fn, enum CodingScheme cs)
 		int payload_written = 0;
 
 		if (m_llc.frame_length() == 0) {
-			/* nothing to sent - delay the release of the TBF */
+			/* It is not clear, when the next real data will
+			 * arrive, so request a DL ack/nack now */
+			request_dl_ack();
 
 			int space = block_data_len - write_offset;
+
+			if (num_chunks != 0) {
+				/* Nothing to send, and we already put some data in
+				 * rlcmac data block, we are done */
+				LOGPTBFDL(this, LOGL_DEBUG,
+					  "LLC queue completely drained and there's "
+					  "still %d free bytes in rlcmac data block\n", space);
+				if (mcs_is_edge(cs)) {
+					/* in EGPRS there's no M bit, so we need
+					 * to flag padding with LI=127 */
+					Encoding::rlc_data_to_dl_append_egprs_li_padding(rdbi,
+											 &write_offset,
+											 &num_chunks,
+											 data);
+				}
+				break;
+			}
+
+			/* Nothing to send from upper layers (LLC), but still
+			 * requested to send something to MS to delay the
+			 * release of the TBF. See 3GPP TS 44.060 9.3.1a
+			 * "Delayed release of downlink Temporary Block Flow" */
 			/* A header will need to by added, so we just need
 			 * space-1 octets */
 			m_llc.put_dummy_frame(space - 1);
@@ -704,10 +728,6 @@ int gprs_rlcmac_dl_tbf::create_new_bsn(const uint32_t fn, enum CodingScheme cs)
 			/* The data just drained, store the current fn */
 			if (m_last_dl_drained_fn < 0)
 				m_last_dl_drained_fn = fn;
-
-			/* It is not clear, when the next real data will
-			 * arrive, so request a DL ack/nack now */
-			request_dl_ack();
 
 			LOGPTBFDL(this, LOGL_DEBUG,
 				  "Empty chunk, added LLC dummy command of size %d, drained_since=%d\n",

src/encoding.cpp
@@ -1397,9 +1397,10 @@ static Encoding::AppendResult rlc_data_to_dl_append_gprs(
 			"larger than space (%d) left in block: copy "
 			"only remaining space, and we are done\n",
 			chunk, space);
-		/* block is filled, so there is no extension */
-		if (e_pointer)
-			*e_pointer |= 0x01;
+		if (e_pointer) {
+			/* LLC frame not finished, so there is no extension octet */
+			*e_pointer |= 0x02; /* set previous M bit = 1 */
+		}
 		/* fill only space */
 		llc->consume(data, space);
 		if (count_payload)
@@ -1438,6 +1439,10 @@ static Encoding::AppendResult rlc_data_to_dl_append_gprs(
 		if (delimiter != data)
 			memmove(delimiter + 1, delimiter,
 				data - delimiter);
+		if (e_pointer) {
+			*e_pointer &= 0xfe; /* set previous E bit = 0 */
+			*e_pointer |= 0x02; /* set previous M bit = 1 */
+		}
 		data++;
 		(*offset)++;
 		space--;
@@ -1465,12 +1470,16 @@ static Encoding::AppendResult rlc_data_to_dl_append_gprs(
 	/* make space for delimiter */
 	if (delimiter != data)
 		memmove(delimiter + 1, delimiter, data - delimiter);
+	if (e_pointer) {
+		*e_pointer &= 0xfe; /* set previous E bit = 0 */
+		*e_pointer |= 0x02; /* set previous M bit = 1 */
+	}
 	data++;
 	(*offset)++;
 	space--;
 	/* add LI to delimit frame */
 	li = (struct rlc_li_field *)delimiter;
-	li->e = 0; /* Extension bit, maybe set later */
+	li->e = 1; /*  not more extension, maybe set later */
 	li->m = 0; /* will be set later, if there is more LLC data */
 	li->li = chunk; /* length of chunk */
 	rdbi->e = 0; /* 0: extensions present */
@@ -1483,22 +1492,19 @@ static Encoding::AppendResult rlc_data_to_dl_append_gprs(
 	space -= chunk;
 	(*offset) += chunk;
 	/* if we have more data and we have space left */
-	if (space > 0 && !is_final) {
-		li->m = 1; /* we indicate more frames to follow */
+	if (space > 0 && !is_final)
 		return Encoding::AR_COMPLETED_SPACE_LEFT;
-	}
+
 	/* if we don't have more LLC frames */
 	if (is_final) {
 		LOGP(DRLCMACDL, LOGL_DEBUG, "-- Final block, so we "
 			"done.\n");
-		li->e = 1; /* we cannot extend */
 		rdbi->cv = 0;
 		return Encoding::AR_COMPLETED_BLOCK_FILLED;
 	}
 	/* we have no space left */
 	LOGP(DRLCMACDL, LOGL_DEBUG, "-- No space left, so we are "
 		"done.\n");
-	li->e = 1; /* we cannot extend */
 	return Encoding::AR_COMPLETED_BLOCK_FILLED;
 }
 
@@ -1594,7 +1600,7 @@ static Encoding::AppendResult rlc_data_to_dl_append_egprs(
 	space     -= 1;
 	/* add LI to delimit frame */
 	li = (struct rlc_li_field_egprs *)delimiter;
-	li->e = 1; /* Extension bit, maybe set later */
+	li->e = 1; /* not more extension, maybe set later */
 	li->li = chunk; /* length of chunk */
 	/* tell previous extension header about the new one */
 	if (prev_li)
@@ -1611,56 +1617,26 @@ static Encoding::AppendResult rlc_data_to_dl_append_egprs(
 	space -= chunk;
 	(*offset) += chunk;
 	/* if we have more data and we have space left */
-	if (space > 0) {
-		if (!is_final)
+	if (!is_final) {
+		if (space > 0) {
 			return Encoding::AR_COMPLETED_SPACE_LEFT;
-
+		} else {
+			/* we have no space left */
+			LOGP(DRLCMACDL, LOGL_DEBUG, "-- No space left, so we are "
+				"done.\n");
+			return Encoding::AR_COMPLETED_BLOCK_FILLED;
+		}
+	} else {
 		/* we don't have more LLC frames */
-		/* We will have to add another chunk with filling octets */
-		LOGP(DRLCMACDL, LOGL_DEBUG,
-			"-- There is remaining space (%d): add filling byte chunk\n",
-			space);
-
-		if (delimiter != data)
-			memmove(delimiter + 1, delimiter, data - delimiter);
-
-		data      += 1;
-		(*offset) += 1;
-		space     -= 1;
-
-		/* set filling bytes extension */
-		li = (struct rlc_li_field_egprs *)delimiter;
-		li->e = 1;
-		li->li = 127;
-
-		/* tell previous extension header about the new one */
-		if (prev_li)
-			prev_li->e = 0;
-
-		delimiter++;
-		(*num_chunks)++;
-
+		LOGP(DRLCMACDL, LOGL_DEBUG, "-- Final block, so we are done.\n");
 		rdbi->cv = 0;
-
-		LOGP(DRLCMACDL, LOGL_DEBUG, "-- Final block, so we "
-			"are done.\n");
-
-		*offset = rdbi->data_len;
+		if (space > 0)
+			Encoding::rlc_data_to_dl_append_egprs_li_padding(rdbi,
+									 offset,
+									 num_chunks,
+									 data_block);
 		return Encoding::AR_COMPLETED_BLOCK_FILLED;
 	}
-
-	if (is_final) {
-		/* we don't have more LLC frames */
-		LOGP(DRLCMACDL, LOGL_DEBUG, "-- Final block, so we "
-			"are done.\n");
-		rdbi->cv = 0;
-		return Encoding::AR_COMPLETED_BLOCK_FILLED;
-	}
-
-	/* we have no space left */
-	LOGP(DRLCMACDL, LOGL_DEBUG, "-- No space left, so we are "
-		"done.\n");
-	return Encoding::AR_COMPLETED_BLOCK_FILLED;
 }
 
 /*!
@@ -1695,6 +1671,39 @@ Encoding::AppendResult Encoding::rlc_data_to_dl_append(
 	OSMO_ASSERT(mcs_is_valid(cs));
 
 	return AR_NEED_MORE_BLOCKS;
+}
+
+void Encoding::rlc_data_to_dl_append_egprs_li_padding(
+	const struct gprs_rlc_data_block_info *rdbi,
+	int *offset, int *num_chunks, uint8_t *data_block)
+{
+	struct rlc_li_field_egprs *li;
+	struct rlc_li_field_egprs *prev_li;
+	uint8_t *delimiter, *data;
+
+	LOGP(DRLCMACDL, LOGL_DEBUG, "Adding LI=127 to signal padding\n");
+
+	data = data_block + *offset;
+	delimiter = data_block + *num_chunks;
+	prev_li = (struct rlc_li_field_egprs *)(*num_chunks ? delimiter - 1 : NULL);
+
+	/* we don't have more LLC frames */
+	/* We will have to add another chunk with filling octets */
+
+	if (delimiter != data)
+		memmove(delimiter + 1, delimiter, data - delimiter);
+
+	/* set filling bytes extension */
+	li = (struct rlc_li_field_egprs *)delimiter;
+	li->e = 1;
+	li->li = 127;
+
+	/* tell previous extension header about the new one */
+	if (prev_li)
+		prev_li->e = 0;
+
+	(*num_chunks)++;
+	*offset = rdbi->data_len;
 }
 
 /*

src/encoding.h
@@ -104,4 +104,7 @@ public:
 		struct gprs_rlc_data_block_info *rdbi, enum CodingScheme cs,
 		gprs_llc *llc, int *offset, int *num_chunks,
 		uint8_t *data, bool is_final, int *count_payload);
+	static void rlc_data_to_dl_append_egprs_li_padding(
+		const struct gprs_rlc_data_block_info *rdbi,
+		int *offset, int *num_chunks, uint8_t *data_block);
 };

commit 8afc6bad806ac6255a04f0aae8d01a9249a05b1f

tbf_dl: Fix m_last_dl_drained_fn not set under some conditions
Old commit getting rid of LLC UI dummy and updating create_new_bsn()
function introduced a bug by not moving update of value m_last_dl_drained_fn
prior to a new break introduced.
As a result, the value is not updated in the case LLC queue becomes
drained but last few bytes are drained at exactly that moment.
Furthermore, then the IDLE tbf timer (X2031, keep_open())) returns always
true since according to it the drain never happened.

The impact of the bug is basically delaying a bit more than expected the
time the TBF stays in IDLE state with the TBF release process yet
to be started.

Related: OS#4849
Fixes: 7d0f9a0ec383fcfca934731bd6979b6be6629c90
Change-Id: I7420aeffda3500bcdc990291e4a56511af433ff9

src/tbf_dl.cpp
@@ -684,6 +684,10 @@ int gprs_rlcmac_dl_tbf::create_new_bsn(const uint32_t fn, enum CodingScheme cs)
 		int payload_written = 0;
 
 		if (llc_frame_length(&m_llc) == 0) {
+			/* The data just drained, store the current fn */
+			if (m_last_dl_drained_fn < 0)
+				m_last_dl_drained_fn = fn;
+
 			/* It is not clear, when the next real data will
 			 * arrive, so request a DL ack/nack now */
 			request_dl_ack();
@@ -714,10 +718,6 @@ int gprs_rlcmac_dl_tbf::create_new_bsn(const uint32_t fn, enum CodingScheme cs)
 			/* A header will need to by added, so we just need
 			 * space-1 octets */
 			m_llc.put_dummy_frame(space - 1);
-
-			/* The data just drained, store the current fn */
-			if (m_last_dl_drained_fn < 0)
-				m_last_dl_drained_fn = fn;
 
 			LOGPTBFDL(this, LOGL_DEBUG,
 				  "Empty chunk, added LLC dummy command of size %d, drained_since=%d\n",

tests/tbf/TbfTest.err
@@ -431,8 +431,8 @@ TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) V(B): (V(A)=21)""(V(S)-1=20)  A=Ack
 Scheduling data message at RTS for DL TFI=0 (TRX=0, TS=4) prio=4 mcs_mode_restrict=EGPRS
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) downlink (V(A)==21 .. V(S)==21) mcs_mode_restrict=EGPRS
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Sending new dummy block at BSN 21, CS=CS-1
-TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Empty chunk, added LLC dummy command of size 19, drained_since=0
-TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Keep idle TBF open: 0/43 -> yes
+TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Empty chunk, added LLC dummy command of size 19, drained_since=4
+TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Keep idle TBF open: 4/43 -> yes
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Complete DL frame, len=19
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) data block (BSN 21, CS-1): 4d 43 c0 01 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) need_padding 0 spb_status 0 spb 0 (BSN1 21 BSN2 -1)
@@ -448,8 +448,8 @@ TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) V(B): (V(A)=22)""(V(S)-1=21)  A=Ack
 Scheduling data message at RTS for DL TFI=0 (TRX=0, TS=4) prio=4 mcs_mode_restrict=EGPRS
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) downlink (V(A)==22 .. V(S)==22) mcs_mode_restrict=EGPRS
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Sending new dummy block at BSN 22, CS=CS-1
-TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Empty chunk, added LLC dummy command of size 19, drained_since=108
-TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Keep idle TBF open: 108/43 -> no
+TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Empty chunk, added LLC dummy command of size 19, drained_since=112
+TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Keep idle TBF open: 112/43 -> no
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) Complete DL frame, len=19
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FLOW) changes state from FLOW to FINISHED
 TBF(TFI=0 TLLI=0xffeeddcc DIR=DL STATE=FINISHED) data block (BSN 22, CS-1): 4d 43 c0 01 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 2b 