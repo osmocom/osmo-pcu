eca828b8f967aa6eabf09aeb9b1bc46c35c8d808
Pau Espin Pedrol <pespin@sysmocom.de>
2023-07-14 19:40:29 +0200

    oc2g: Fix multiple definitions of arrays
    These errors started appearing lately when upgrading  builders to
    debian12, with a newer gcc version.
    
    Change-Id: I0046a6f4fb3979710b4cd5222de599d9aaac227b

--- src/osmo-bts-oc2g/oc2gbts.h
+++ src/osmo-bts-oc2g/oc2gbts.h
@@ -23,24 +23,24 @@ enum l1prim_type {
 };
 
 enum l1prim_type oc2gbts_get_l1prim_type(GsmL1_PrimId_t id);
-const struct value_string oc2gbts_l1prim_names[GsmL1_PrimId_NUM+1];
+extern const struct value_string oc2gbts_l1prim_names[GsmL1_PrimId_NUM+1];
 GsmL1_PrimId_t oc2gbts_get_l1prim_conf(GsmL1_PrimId_t id);
 
 enum l1prim_type oc2gbts_get_sysprim_type(Oc2g_PrimId_t id);
-const struct value_string oc2gbts_sysprim_names[Oc2g_PrimId_NUM+1];
+extern const struct value_string oc2gbts_sysprim_names[Oc2g_PrimId_NUM+1];
 Oc2g_PrimId_t oc2gbts_get_sysprim_conf(Oc2g_PrimId_t id);
 
-const struct value_string oc2gbts_l1sapi_names[GsmL1_Sapi_NUM+1];
-const struct value_string oc2gbts_l1status_names[GSML1_STATUS_NUM+1];
+extern const struct value_string oc2gbts_l1sapi_names[GsmL1_Sapi_NUM+1];
+extern const struct value_string oc2gbts_l1status_names[GSML1_STATUS_NUM+1];
 
-const struct value_string oc2gbts_tracef_names[29];
-const struct value_string oc2gbts_tracef_docs[29];
+extern const struct value_string oc2gbts_tracef_names[29];
+extern const struct value_string oc2gbts_tracef_docs[29];
 
-const struct value_string oc2gbts_tch_pl_names[15];
+extern const struct value_string oc2gbts_tch_pl_names[15];
 
-const struct value_string oc2gbts_clksrc_names[10];
+extern const struct value_string oc2gbts_clksrc_names[10];
 
-const struct value_string oc2gbts_dir_names[6];
+extern const struct value_string oc2gbts_dir_names[6];
 
 enum pdch_cs {
 	PDCH_CS_1,
@@ -59,6 +59,6 @@ enum pdch_cs {
 	_NUM_PDCH_CS
 };
 
-const uint8_t pdch_msu_size[_NUM_PDCH_CS];
+extern const uint8_t pdch_msu_size[_NUM_PDCH_CS];
 
 #endif /* LC15BTS_H */
